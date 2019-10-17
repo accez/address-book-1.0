15d7d16816b83ffb7d20210fd17d4f88b977b559
SimonPalmgren <simon.palmgren91@gmail.com>
2019-10-17 10:31:12 +0200

    listners for adding and removing contacts

--- src/js/Contact.js
+++ src/js/Contact.js
@@ -1,27 +1,25 @@
-class Contact extends Index {
-  constructor() {
-    super()
-    this.saveButtonListner = listen('click', '.save-button', e => {
-      this.saveContact()
-    });
-  }
-  saveContact() {
-    const nameInput = document.querySelector('#name').value
-    const emailInput = document.querySelector('#email').value
-    const phoneNumberInput = document.querySelector('#phonenumber').value
-    let data = {
-      name: nameInput,
-      email: emailInput,
-      phonenumber: phoneNumberInput
-    }
-    store.push(data)
-    store.forEach((value, key) => {
-      value.id = key++
-    })
-    store.save()
-    this.resetForm()
-  }
-  resetForm() {
-    document.querySelector('#myForm').reset()
+class Contact {
+  constructor(name, email, phone) {
+    this.name = name
+    this.email = email
+    this.phone = phone
   }
 }
+const addressBook = new AddressBook()
+
+listen('click', '.save-button', e => {
+  let emailArr = []
+  let phoneArr = []
+  const nameInput = document.querySelector('#name').value
+  const emailInput = document.querySelector('#email').value
+  const phoneNumberInput = document.querySelector('#phonenumber').value
+  emailArr.push(emailInput)
+  phoneArr.push(phoneNumberInput)
+  addressBook.add(new Contact(nameInput, emailArr, phoneArr))
+  addressBook.renderContacts()
+});
+
+listen('click', '.remove', e => {
+  let id = e.target.getAttribute('key')
+  addressBook.delete(store,id)
+});
