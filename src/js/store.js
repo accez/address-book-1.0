36e006ad6796434c8047d8cf2259624789b017c1
SimonPalmgren <simon.palmgren91@gmail.com>
2019-10-17 10:24:27 +0200

    change store so it does not save over itself

--- src/js/store.js
+++ src/js/store.js
@@ -1,13 +1,17 @@
-//Local storage
-// Initial read
 let store;
+let initLocalDatabase = false;
 try {
- store = JSON.parse(localStorage.store);
+  store = JSON.parse(localStorage.store);
 }
-catch(e){
- store = [];
+catch (e) {
+  store = {};
+  initLocalDatabase = true;
 }
- 
-store.save = function(){
+store.save = function () {
   localStorage.store = JSON.stringify(this);
 };
+// Run an save if DB is not present in localstorage at boot
+initLocalDatabase ? store.save() : null;
+
+store.contacts = store.contacts || []
+store.save()
